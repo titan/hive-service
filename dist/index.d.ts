f7558fb7e31585d60fe202fc94b38f7aca47b974
Titan <howay.tan@gmail.com>
2016-12-30 12:01:37 +0800

    Add number type to zrevrangebyscoreAsync

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -24,7 +24,7 @@ declare module "redis" {
         zaddAsync(key: string, score: number, member: string | number | Buffer): Promise<any>;
         zcountAsync(key: string, min: string | number, max: string | number): Promise<any>;
         zrangeAsync(key: string, start: number, stop: number): Promise<any>;
-        zrevrangebyscoreAsync(key: string, start: number, stop: number, limit?: string, offset?: number, count?: number): Promise<any>;
+        zrevrangebyscoreAsync(key: string, start: number | string, stop: number | string, limit?: string, offset?: number, count?: number): Promise<any>;
     }
     interface Multi extends NodeJS.EventEmitter {
         execAsync(): Promise<any>;

faf4b676fcf089301e5ce1b2f2a645add8ec7f58
Titan <howay.tan@gmail.com>
2017-01-19 18:19:20 +0800

    Cache fib result

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -111,7 +111,8 @@ export declare class Service {
     registerProcessor(processor: Processor): void;
     run(): void;
 }
-export declare function fib(n: number): any;
+export declare function fib(n: number): number;
+export declare function fiball(n: number): number;
 export declare function wait_for_response(cache: RedisClient, reply: string, rep: ((result: any) => void), retry?: number): void;
 export declare function set_for_response(cache: RedisClient, key: string, value: any, timeout?: number): Promise<any>;
 export declare function rpc<T>(domain: string, addr: string, uid: string, fun: string, ...args: any[]): Promise<T>;

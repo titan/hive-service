64a31ef5beec74725f8c989a2e5e90fecd6a6140
Titan <howay.tan@gmail.com>
2017-04-26 12:00:12 +0800

    Add zrangebyscoreAsync

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -36,11 +36,12 @@ declare module "redis" {
         smembersAsync(key: string): Promise<any>;
         zaddAsync(key: string, score: number, member: string | number | Buffer): Promise<any>;
         zcountAsync(key: string, min: string | number, max: string | number): Promise<any>;
-        zrangeAsync(key: string, start: number, stop: number): Promise<any>;
+        zrangeAsync(key: string, start: number, stop: number): Promise<any[]>;
+        zrangebyscoreAsync(key: string, min: number | string, max: number | string, limit?: string, offset?: number, count?: number): Promise<any[]>;
         zremAsync(key: string, value: any): Promise<any>;
         zremrangebyscoreAsync(key: string, start: number | string, stop: number | string): Promise<any>;
-        zrevrangeAsync(key: string, start: number, stop: number): Promise<any>;
-        zrevrangebyscoreAsync(key: string, start: number | string, stop: number | string, limit?: string, offset?: number, count?: number): Promise<any>;
+        zrevrangeAsync(key: string, start: number, stop: number): Promise<any[]>;
+        zrevrangebyscoreAsync(key: string, start: number | string, stop: number | string, limit?: string, offset?: number, count?: number): Promise<any[]>;
     }
     interface Multi extends NodeJS.EventEmitter {
         execAsync(): Promise<any>;
